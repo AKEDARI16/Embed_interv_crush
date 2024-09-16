967c016d13e902e3cfdcf929c6f16c3a46624032
abhilash <tbs.abhilash@gmail.com>
2024-09-16 11:49:12 +0530

    updated binary search code

--- Searching_techs/binary_search.c
+++ Searching_techs/binary_search.c
@@ -3,34 +3,30 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int binarySearch(int arr, int size, int key);
+int binarySearch(int *arr, int size, int key);
 
-int binarySearch(int arr, int size, int key)
+int binarySearch(int *arr, int size, int key)
 {
     int low = 0, high = size - 1;
-    int mid = 0;
-
     while(low <= high){
-        mid = (low + high)/2;
+        int mid = (low + high)/2;
         if(arr[mid] == key){
             return mid;
-        }else if(){
-
+        }else if(key > arr[mid]){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
         }
     }
 }
 
-
-
-
 int main()
 {
-
     int arr[] = {2,3,4,5,6,7,8,9};
     int size = sizeof(arr)/sizeof(arr[0]);
-    int key = 6;
+    int key = 8;
     int result = binarySearch(arr, size, key);
-
+    printf(" the index of the key is : %d\n",result);
     return 0;
 }
 
